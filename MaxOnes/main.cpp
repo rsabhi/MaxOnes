1bbdc9bf00fb6dd2056822a1c9d93f2ea41d4bcd
rsabhi <abhi.rs@gmail.com>
2018-03-25 23:02:55 +1100

    code updated

--- MaxOnes/main.cpp
+++ MaxOnes/main.cpp
@@ -7,9 +7,30 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int nMax_Ones = 0;
+        int n = (int)nums.size(); // size of the vector array
+        for (int i= 0; i<n; ++i) {
+            if(nums[i] == 1) {
+                int curr_len = 1;
+                int r = i + 1;
+                while(r<n && nums[r] == 1) {
+                    ++curr_len;
+                    ++r;
+                }
+                nMax_Ones = max(nMax_Ones, curr_len);
+            }
+            
+        } // for loop
+        return nMax_Ones;
+    }
+};
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
